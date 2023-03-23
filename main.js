8488534e8d3ca01147f363b1b67a10d8407d2f4e
thenannyategg <128714823+thenannyategg@users.noreply.github.com>
2023-03-23 11:23:41 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,17 @@
+import { addArgentinaHolidays, calculateCourseEndDate } from './calendarUtils.js';
+
+document.getElementById('course-form').addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const startDate = document.getElementById('start-date').value;
+  const numberOfClasses = document.getElementById('number-of-classes').value;
+
+  const events = [];
+  addArgentinaHolidays(events);
+
+  const endDate = calculateCourseEndDate(startDate, numberOfClasses, events);
+
+  document.getElementById('result-start-date').innerText = startDate;
+  document.getElementById('result-end-date').innerText = endDate;
+  document.getElementById('result').classList.remove('hidden');
+});
