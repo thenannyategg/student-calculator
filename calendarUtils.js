a23cda34d3fffca9e8d449e820133a39a591384b
thenannyategg <128714823+thenannyategg@users.noreply.github.com>
2023-03-23 11:22:59 -0300

    Create calendarUtils.js

---
+++ calendarUtils.js
@@ -0,0 +1,65 @@
+const argentinaHolidays = [
+  { date: '2023-01-01', title: 'New Year\'s Day' },
+  { date: '2023-02-20', title: 'Carnival Monday' },
+  { date: '2023-02-21', title: 'Carnival Tuesday' },
+  { date: '2023-03-24', title: 'Day of Remembrance for Truth and Justice' },
+  // Add other holidays here
+];
+
+function addArgentinaHolidays(events) {
+  argentinaHolidays.forEach(holiday => {
+    if (!events.find(event => event.date === holiday.date)) {
+      events.push(holiday);
+    }
+  });
+}
+
+function calculateCourseEndDate(startDate, numberOfClasses, events) {
+  const courseStart = moment(startDate);
+  const holidays = events.map(event => event.date);
+
+  let classesHeld = 0;
+  let currentDate = course
+const argentinaHolidays = [
+  { date: '2023-01-01', title: 'New Year\'s Day' },
+  { date: '2023-02-20', title: 'Carnival Monday' },
+  { date: '2023-02-21', title: 'Carnival Tuesday' },
+  { date: '2023-03-24', title: 'Day of Remembrance for Truth and Justice' },
+  // Add other holidays here
+];
+
+function addArgentinaHolidays(events) {
+  argentinaHolidays.forEach(holiday => {
+    if (!events.find(event => event.date === holiday.date)) {
+      events.push(holiday);
+    }
+  });
+}
+
+function calculateCourseEndDate(startDate, numberOfClasses, events) {
+  const courseStart = moment(startDate);
+  const holidays = events.map(event => event.date);
+
+  let classesHeld = 0;
+  let currentDate = courseStart.clone();
+
+  while (classesHeld < numberOfClasses) {
+    const dayOfWeek = currentDate.day();
+    const isHoliday = holidays.includes(currentDate.format('YYYY-MM-DD'));
+
+    if (dayOfWeek !== 0 && dayOfWeek !== 6 && !isHoliday) {
+      classesHeld++;
+    }
+
+    if (classesHeld < numberOfClasses) {
+      currentDate.add(1, 'days');
+    }
+  }
+
+  return currentDate.format('YYYY-MM-DD');
+}
+
+export {
+  addArgentinaHolidays,
+  calculateCourseEndDate,
+};
